a9056fe8d2efe3228ec66b1167c532ca7d713822
Fred Morrison <fpmorrison@verizon.net>
2017-06-01 16:46:51 -0400

    made size of the calculator proportional to screen size

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,16 @@ public class Main
 
     public static void main(String[] args)
     {
+        // make size decisions based on the current screen size
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+        int xSize = (int) screenSize.getWidth();
+        int ySize = (int) screenSize.getHeight();
+
         JFrame frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(1, 1));
-        Dimension size = new Dimension(700, 900);
+        Dimension size = new Dimension(xSize / 3,  ySize * 2 / 3);
         frame.setSize(size);
         frame.setPreferredSize(size);
         frame.getContentPane().add(new CalculatorGrid(frame));
